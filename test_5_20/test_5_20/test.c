78be78a3f40e7f7c253974f3bf82e627a33ee34e
XiaoYang202 <yzz19991023>
2022-05-20 22:29:08 +0800

    测试hellow

---
+++ test_5_20/test_5_20/test.c
@@ -0,0 +1,13 @@
+#include<stdio.h>//包含一个叫stdio.h的文件
+//std-标准 standard input output
+//int是整型的意思
+//main前面的int表示main函数调用返回一个整型值
+int main()//主函数-程序的入口（必有且唯一）
+{
+	printf("hellow\n");//这里完成任务
+	//函数printf()——打印函数
+	//\n表示换行
+	//c语言本身提供的函数叫库函数
+	//#是一种指令形式
+	return 0;//返回整数0
+}
